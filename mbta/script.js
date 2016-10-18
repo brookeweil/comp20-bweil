a79ebfc752744a0b98d53ae7445796275e315aae
Brooke Weil <brooke.marie.weil@gmail.com>
2016-10-18 12:40:20 -0400

    Added listeners and info windows for each station

--- mbta/script.js
+++ mbta/script.js
@@ -9,15 +9,14 @@
             };
     var map;
     var marker;
-    var infowindow = new google.maps.InfoWindow({
-       
-    });
+    var infowindow = new google.maps.InfoWindow();
 
     var stationMarkers = [];
     var branch1Coords = [];
     var branch2Coords = [];
     var branch1Path;
     var branch2Path;
+    var trainStops;
     var trainIcon = {
         url: "train.png",
         size: new google.maps.Size(100, 100),
@@ -26,33 +25,34 @@
     };
 
     // Hardcoding the station locations and names
-    stationMarkers [1]  = {name: "Alewife",         lat:   42.395428,  lng:         -71.142483}
-    stationMarkers [2]  = {name: "Davis",  lat:    42.39674,  lng:         -71.121815}
-    stationMarkers [3]  = {name: "Porter Square    ",  lat:     42.3884,  lng: -71.11914899999999}
-    stationMarkers [4]  = {name: "Harvard Square   ",  lat:   42.373362,  lng:         -71.118956}
-    stationMarkers [5]  = {name: "Central Square   ",  lat:   42.365486,  lng:         -71.103802}
-    stationMarkers [6]  = {name: "Kendall/MIT      ",  lat: 42.36249079,  lng:       -71.08617653}
-    stationMarkers [7]  = {name: "Charles/MGH      ",  lat:   42.361166,  lng:         -71.070628}
-    stationMarkers [8]  = {name: "Park Street      ",  lat: 42.35639457,  lng:        -71.0624242}
-    stationMarkers [9]  = {name: "Downtown Crossing",  lat:   42.355518,  lng:         -71.060225}
-    stationMarkers [10] = {name: "South Station    ",  lat: 42.352271  ,  lng: -71.05524200000001}
-    stationMarkers [11] = {name: "Broadway         ",  lat:   42.342622,  lng:         -71.056967}
-    stationMarkers [12] = {name: "Andrew           ",  lat:  42.330154 ,  lng:         -71.057655}
-    stationMarkers [13] = {name: "JFK/UMass        ",  lat:   42.320685,  lng:         -71.052391} ///
-    stationMarkers [14] = {name: "North Quincy     ",  lat:   42.275275,  lng:         -71.029583}
-    stationMarkers [15] = {name: "Wollaston        ",  lat:  42.2665139,  lng:        -71.0203369}
-    stationMarkers [16] = {name: "Quincy Center    ",  lat:   42.251809,  lng:         -71.005409}
-    stationMarkers [17] = {name: "Quincy Adams     ",  lat:   42.233391,  lng:         -71.007153}
-    stationMarkers [18] = {name: "Braintree        ",  lat:  42.2078543,  lng:        -71.0011385}
-    stationMarkers [19] = {name: "Savin Hill       ",  lat:    42.31129,  lng:         -71.053331}
-    stationMarkers [20] = {name: "Fields Corner    ",  lat:   42.300093,  lng:         -71.061667}
-    stationMarkers [21] = {name: "Shawmut          ",  lat: 42.29312583,  lng: -71.06573796000001}
-    stationMarkers [22] = {name: "Ashmont          ",  lat:   42.284652,  lng: -71.06448899999999}
+    stationMarkers [1]  = {name: "Alewife",          lat:   42.395428,  lng:         -71.142483}
+    stationMarkers [2]  = {name: "Davis",            lat:    42.39674,  lng:         -71.121815}
+    stationMarkers [3]  = {name: "Porter Square",    lat:     42.3884,  lng: -71.11914899999999}
+    stationMarkers [4]  = {name: "Harvard Square",   lat:   42.373362,  lng:         -71.118956}
+    stationMarkers [5]  = {name: "Central Square",   lat:   42.365486,  lng:         -71.103802}
+    stationMarkers [6]  = {name: "Kendall/MIT",      lat: 42.36249079,  lng:       -71.08617653}
+    stationMarkers [7]  = {name: "Charles/MGH",      lat:   42.361166,  lng:         -71.070628}
+    stationMarkers [8]  = {name: "Park Street",      lat: 42.35639457,  lng:        -71.0624242}
+    stationMarkers [9]  = {name: "Downtown Crossing",lat:   42.355518,  lng:         -71.060225}
+    stationMarkers [10] = {name: "South Station",    lat: 42.352271  ,  lng: -71.05524200000001}
+    stationMarkers [11] = {name: "Broadway",         lat:   42.342622,  lng:         -71.056967}
+    stationMarkers [12] = {name: "Andrew",           lat:  42.330154 ,  lng:         -71.057655}
+    stationMarkers [13] = {name: "JFK/UMass",        lat:   42.320685,  lng:         -71.052391} 
+    stationMarkers [14] = {name: "North Quincy",     lat:   42.275275,  lng:         -71.029583}
+    stationMarkers [15] = {name: "Wollaston",        lat:  42.2665139,  lng:        -71.0203369}
+    stationMarkers [16] = {name: "Quincy Center",    lat:   42.251809,  lng:         -71.005409}
+    stationMarkers [17] = {name: "Quincy Adams",     lat:   42.233391,  lng:         -71.007153}
+    stationMarkers [18] = {name: "Braintree",        lat:  42.2078543,  lng:        -71.0011385}
+    stationMarkers [19] = {name: "Savin Hill",       lat:    42.31129,  lng:         -71.053331}
+    stationMarkers [20] = {name: "Fields Corner",    lat:   42.300093,  lng:         -71.061667}
+    stationMarkers [21] = {name: "Shawmut",          lat: 42.29312583,  lng: -71.06573796000001}
+    stationMarkers [22] = {name: "Ashmont",          lat:   42.284652,  lng: -71.06448899999999}
         
     function init()
     {
         map = new google.maps.Map(document.getElementById("map"), myOptions);
         getMyLocation();
+        renderStations();
     }
     
     function getMyLocation() {
@@ -70,9 +70,6 @@
 
     // From StackOverflow user talkol, at 
     // http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
-    // Number.prototype.toRad = function() {
-    //    return this * Math.PI / 180;
-    // }
     function toRad(num){
         return ((num * Math.PI) / 180);
 
@@ -92,7 +89,6 @@
                         Math.sin(dLon/2) * Math.sin(dLon/2);  
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         var d = R * c; 
-
 
         return(d);
     }
@@ -114,17 +110,33 @@
         return {station: stationMarkers[indexOfNearest], dist: shortestDist};
     }
 
-    function renderStations(){
+    function initStation(station, index){
 
-        // Add stations to map
-        for (var i in stationMarkers){
-            stationMarkers[i].marker = new google.maps.Marker({
-                position: {lat: stationMarkers[i].lat, lng: stationMarkers[i].lng},
-                title: stationMarkers[i].name,
+        // Add station to map
+            station.marker = new google.maps.Marker({
+                position: {lat: station.lat, lng: station.lng},
+                title: station.name,
                 icon: trainIcon
             });
-            stationMarkers[i].marker.setMap(map);
-        }
+            station.marker.setMap(map);
+
+            // Add info windows with listeners
+            station.info = new google.maps.InfoWindow({
+                content: station.name
+            });
+            console.log ("Adding listener for " + station.marker.title);
+            station.marker.addListener('click', function() {
+                station.info.open(map, station.marker);
+            });
+    }
+
+    function renderStations(){
+
+        // Load train stop predictions from JSON
+        loadTrainStops();
+
+        // Add stations to map
+        stationMarkers.forEach(initStation);
 
         // Create polyline for branch 1
         for (var j = 1; j < 19; j++){
@@ -158,6 +170,31 @@
         branch2Path.setMap(map);
     }
 
+    function loadTrainStops() {
+        // Step 1: create an instance of XMLHttpRequest
+        var request = new XMLHttpRequest();
+        // Step 2: Make request to remote resource
+        // NOTE: https://messagehub.herokuapp.com has cross-origin resource sharing enabled
+        request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+        // Step 3: Create handler function to do something with data in response
+        request.onreadystatechange = funex;
+        // Step 4: Send the request
+        request.send();
+    }
+    function funex() {
+        console.log("The data is => " + request.responseText);
+        // Step 5: When data is received, get it and do something with it
+        // if (request.readyState == 4 && request.status == 200) {
+        if (request.status == 200) {
+            console.log("Saving the data");
+            // Step 5A: get the response text
+            var theData = request.responseText;
+            // Step 5B: parse the text into JSON
+            trainStops = JSON.parse(theData);
+        }
+    }
+
+
 
     function renderMap() {
         
@@ -170,7 +207,7 @@
         map.panTo(me);
 
         // Put stations and routes on map
-        renderStations();
+        // renderStations();
 
         // Create a marker
         marker = new google.maps.Marker({
@@ -196,7 +233,7 @@
             infowindow.setContent(
                 "<h4> Closest station: </h4> " + closestStation.station.name +
                 "<h4>Distance away: </h4>" + closestStation.dist.toPrecision(1)
-                + " miles"); 
+                + " miles");
         });
     }
 
